f385fd3dbeadeda4edd75b8cb979625f3c9b2312
Joshua Zimmerman <jpz6926@gmail.com>
2025-02-19 23:52:09 -0500

    Set up two-way binding on input text bar, in preparation for implementing form submit action

--- src/components/InputBar/InputBar.jsx
+++ src/components/InputBar/InputBar.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import "../InputBar/InputBar.css";
 
 function InputBar() {
+  const [inputValue, setValue] = useState("");
+
+  const inputTextChanged = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <>
       <div>
-        <input type="text" id="inputTerm" className="input-bar" />
+        <input
+          type="text"
+          id="inputTerm"
+          className="input-bar"
+          value={inputValue}
+          placeholder="Enter iten name or barcode"
+          onChange={inputTextChanged}
+        />
         <button className="submit-button">Submit</button>
       </div>
     </>
