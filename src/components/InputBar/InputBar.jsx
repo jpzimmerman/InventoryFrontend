58c3a583fdb7edea9050f59d738ceec529178ec0
Joshua Zimmerman <jpz6926@gmail.com>
2025-02-24 23:54:46 -0500

    Adds basic input validation to InputBar

--- src/components/InputBar/InputBar.jsx
+++ src/components/InputBar/InputBar.jsx
@@ -4,13 +4,21 @@ import { addInventoryItem } from "../../services/inventoryDataService";
 
 function InputBar() {
   const [inputValue, setValue] = useState("");
+  const [error, setError] = useState("");
 
   const inputTextChanged = (event) => {
     setValue(event.target.value);
   };
 
-  const inputBarSubmit = () => {
-    addInventoryItem({ name: inputValue });
+  const inputBarSubmit = (e) => {
+    e.preventDefault();
+    if (!inputValue) {
+      setError("Input value cannot be empty");
+      return;
+    } else {
+      addInventoryItem({ name: inputValue });
+      setError("");
+    }
   };
 
   return (
@@ -29,6 +37,12 @@ function InputBar() {
             Submit
           </button>
         </form>
+        {error && (
+          <p className="error-text">
+            <i className="fa-solid fa-xmark"></i>
+            {error}
+          </p>
+        )}
       </section>
     </>
   );
